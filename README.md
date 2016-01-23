f18489ca1a2c40e370c0e47b51831a501303a824
Jared Hanson <jaredhanson@gmail.com>
2016-01-22 17:47:17 -0800

    Add README.

---
+++ README.md
@@ -0,0 +1,1 @@
+# Makefiles for Node.js packages
--- main.mk
+++ main.mk
@@ -1,6 +1,5 @@
 __JAREDHANSON_MAKE_NODE_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
-
 include $(__JAREDHANSON_MAKE_NODE_DIR)/lib/test.mk
 include $(__JAREDHANSON_MAKE_NODE_DIR)/lib/test-cov.mk
 include $(__JAREDHANSON_MAKE_NODE_DIR)/lib/lint.mk
