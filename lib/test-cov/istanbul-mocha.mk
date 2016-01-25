8b20c00d25786f1b6a5b61be3d6c55bfdb2d7741
Jared Hanson <jaredhanson@gmail.com>
2016-01-25 10:27:43 -0800

    Document test targets.

--- lib/test-cov/istanbul-mocha.mk
+++ lib/test-cov/istanbul-mocha.mk
@@ -1,8 +1,24 @@
 _MOCHA ?= $(BIN)/_mocha
 
+# Run test suite and report code coverage.
+#
+# This target verifies the software's behavior by running the test suite, while
+# simultaneously collecting code coverage statistics.  The test suite is
+# expected to utilize Mocha[^1] as a test framework.  The code itself will be
+# instrumented by Istanbul[^2] in order to check code coverage.
+#
+# Istanbul can be installed by executing the following command:
+#     $ npm install -g istanbul
+#
+# [^1]: https://mochajs.org/
+# [^2]: https://github.com/gotwarlost/istanbul
 test-cov:
 	$(ISTANBUL) cover $(ISTANBULFLAGS) --dir $(COVERAGEDIR) $(_MOCHA) -- $(MOCHAFLAGS) $(TESTS)
 
+# Clean up code coverage.
+#
+# This target cleans up any reports written while collecting code coverage
+# statistics.
 clean-cov:
 	-rm -r $(COVERAGEDIR)
 
--- lib/test/mocha.mk
+++ lib/test/mocha.mk
@@ -1,5 +1,11 @@
 MOCHA ?= $(BIN)/mocha
 
+# Run test suite.
+#
+# This target verifies the software's behavior by running the test suite.  The
+# test suite is expected to utilize Mocha[^1] as a test framework.
+#
+# [^1]: https://mochajs.org/
 test:
 	$(MOCHA) $(MOCHAFLAGS) $(TESTS)
 
