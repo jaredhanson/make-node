4973583bfa1a94832be2e6bacf4289c8a49e06ed
Jared Hanson <jaredhanson@gmail.com>
2016-01-25 10:56:28 -0800

    Document report-cov target.

--- lib/report-cov/coveralls.mk
+++ lib/report-cov/coveralls.mk
@@ -1,5 +1,15 @@
 COVERALLS ?= coveralls
 
+# Report code coverage.
+#
+# This target sends code coverage reports to Coveralls[^1], where they can be
+# viewed and analyzed.
+#
+# `coveralls`[^2] can be installed by executing the following command:
+#     $ npm install -g coveralls
+#
+# [^1]: https://coveralls.io/
+# [^2]: https://github.com/nickmerwin/node-coveralls
 report-cov:
 	cat $(LCOVFILE) | $(COVERALLS)
 
