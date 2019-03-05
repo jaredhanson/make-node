5d88eece0bba0973d57038744af24f41760ec9e4
Jared Hanson <jaredhanson@gmail.com>
2019-03-05 15:25:51 -0800

    Factor out view-cov and clean-cov targets.

--- lib/cov/istanbul.mk
+++ lib/cov/istanbul.mk
@@ -1,1 +1,14 @@
 ISTANBUL ?= istanbul
+
+view-cov:
+	open $(localstatedir)/cov/lcov-report/index.html
+
+# Clean up code coverage.
+#
+# This target cleans up any reports written while collecting code coverage
+# statistics.
+clean-cov:
+	-rm -r $(localstatedir)/cov
+
+
+.PHONY: view-cov clean-cov
