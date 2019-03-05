46e92d22b30c417124ee4fb4d2642ee695d3344a
Jared Hanson <jaredhanson@gmail.com>
2019-03-04 16:34:05 -0800

    Update JSDoc makefile to use HTML dir.

--- lib/docs/jsdoc.mk
+++ lib/docs/jsdoc.mk
@@ -6,7 +6,12 @@ JSDOC ?= jsdoc
 #
 # JSDoc can be installed by executing the following command:
 #     $ npm install -g jsdoc
-doc: var/doc/index.html
+#
+# This target was developed against jsdoc 3.5.5.  To check the version of jsdoc
+# that is installed on your system, execute the following command:
+#     $ jsdoc -v
+#     JSDoc 3.5.5 (Thu, 14 Sep 2017 02:51:54 GMT)
+doc: $(htmldir)/index.html
 
 
 
@@ -18,7 +23,5 @@ doc: var/doc/index.html
 #
 # Note that only the home page (`index.html`) is used as a target, as that
 # serves as an index of all modules within the package.
-var/doc/index.html: $(SOURCES)
-	$(JSDOC) -d ./var/doc $^
-
-.PHONY: doc
+$(htmldir)/index.html: $(SOURCES)
+	$(JSDOC) $(JSDOCFLAGS) -d $(htmldir) $^
