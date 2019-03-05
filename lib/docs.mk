8d6677e98b7d728d3181db8036c34f49d3756cbb
Jared Hanson <jaredhanson@gmail.com>
2019-03-04 18:14:38 -0800

    Improve selection of doc implementation.

--- lib/docs.mk
+++ lib/docs.mk
@@ -1,5 +1,10 @@
-WITH_MRDOC ?= 1
+__docimpl = jsdoc mr-doc
+__docbin = $(firstword $(filter $(__docimpl),$(WITH) $(notdir $(shell which $(__docimpl)))))
 
-ifdef WITH_MRDOC
-  include $(__JAREDHANSON_MAKE_NODE_DIR)/lib/docs/mr-doc.mk
+ifeq (jsdoc,$(__docbin))
+#$(info Using JSDoc for 'doc'.)
+include $(__JAREDHANSON_MAKE_NODE_DIR)/lib/docs/jsdoc.mk
+else ifeq (mr-doc,$(__docbin))
+#$(info Using Mr. Doc for 'doc'.)
+include $(__JAREDHANSON_MAKE_NODE_DIR)/lib/docs/mr-doc.mk
 endif
