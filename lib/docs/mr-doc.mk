09a34e7a7e08dc8a9b1ae3b0d105b10d46d954cb
Jared Hanson <jaredhanson@gmail.com>
2019-03-04 16:31:22 -0800

    Update Mr. Doc makefile to be file-based.

--- lib/docs/mr-doc.mk
+++ lib/docs/mr-doc.mk
@@ -2,17 +2,23 @@ MRDOC ?= mr-doc
 
 # Generate documentation.
 #
-# This target generates API documentation by processing source code for
-# JSDoc[^1]-style comments.  Mr. Doc[^2] is used to generatate themeable
-# HTML output.
+# This target generates API documentation using Mr. Doc, which supports JSDoc-
+# style comments.
 #
 # Mr. Doc can be installed by executing the following command:
 #     $ npm install -g mr-doc
 #
-# [^1]: http://usejsdoc.org/
-# [^2]: https://github.com/mr-doc/mr-doc
-docs:
-	$(MRDOC) $(MRDOCFLAGS) -s $(SRCDIR) -o $(DOCSDIR)
+# This target was developed against mr-doc 3.2.1.  To check the version of
+# mr-doc that is installed on your system, execute the following command:
+#     $ mr-doc -V
+#     3.2.1
+doc: $(htmldir)/index.html
+
+
+
+
+$(htmldir)/index.html: $(SOURCES)
+	$(MRDOC) $(MRDOCFLAGS) -o $(htmldir) -s $(firstword $(sort $(dir $^)))
 
 # Clean up documentation.
 #
@@ -21,4 +27,4 @@ clean-docs:
 	-rm -r $(DOCSDIR)
 
 
-.PHONY: docs clean-docs
+.PHONY: clean-docs
