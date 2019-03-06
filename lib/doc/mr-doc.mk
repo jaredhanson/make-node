69df9717532859133ca2499e65fb77a03933cde3
Jared Hanson <jaredhanson@gmail.com>
2019-03-06 11:19:22 -0800

    Improve mr-doc targets.

--- lib/doc/mr-doc.mk
+++ lib/doc/mr-doc.mk
@@ -1,9 +1,9 @@
 MRDOC ?= mr-doc
 
-# Generate documentation.
+# Build documentation.
 #
-# This target generates API documentation using Mr. Doc, which supports JSDoc-
-# style comments.
+# This target generates API documentation using [Mr. Doc](https://mr-doc.github.io/),
+# which supports JSDoc-style comments.
 #
 # Mr. Doc can be installed by executing the following command:
 #
@@ -14,28 +14,29 @@ MRDOC ?= mr-doc
 #
 #     $ mr-doc -V
 #     3.2.1
-doc: $(htmldir)/index.html
+.PHONY: doc doc-html
+doc: doc-html
+doc-html: $(htmldir)/index.html
 
 
 
-
-# Generate documentation home page.
+# Build documentation home page.
 #
-# The source code is a prerequisite for the documentation as the documentation
-# is embedded as comments within the source code itself.
+# All source files are a prerequisite for the documentation, which is embedded
+# in comments within the source code itself.
 #
 # Note that only the home page (`index.html`) is used as a target, as that
 # serves as an index of all modules within the package.  As such, a modification
-# to any of the prerequisites will trigger documentation to be regenerated.
+# to any of the source files will cause the home page to be considered out-of
+# date, triggering the documentation to be rebuilt.
 $(htmldir)/index.html: $(SOURCES)
 	$(MRDOC) $(MRDOCFLAGS) -o $(htmldir) -s $(srcdir)
 
-# Clean up documentation.
-#
-# This target cleans up any API documentation generated from source code.
-clean-docs:
+# Delete all files that are created by building documentation.
+.PHONY: clean-doc clean-doc-html
+clean-doc: clean-doc-html
+clean-doc-html:
 	-rm -r $(htmldir)
 
-
-.PHONY: clean-docs
-
+.PHONY: distclean-doc
+distclean-doc: clean-doc
