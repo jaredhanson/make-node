48be2d941f0f3ca5bbdce73fdfa0a8a28e59107f
Jared Hanson <jaredhanson@gmail.com>
2019-03-08 11:28:25 -0800

    Add comments.

--- lib/cov.mk
+++ lib/cov.mk
@@ -1,3 +1,14 @@
+# Inject coverage-related targets.
+#
+# This include introspects the system and injects coverage-related targets based
+# on which program is available.
+#
+# The include locates the supported programs using `which`, and then extracts
+# the name of the program, removing the directory-part of the file name.  The
+# environment variable `WITH` can be specified to override the search order.
+# Because the override can potentially specify unsupported programs, the
+# result is filtered to only supported programs.  The preferred program is then
+# selected and the appropriate targets are included.
 __covimpl = istanbul jscoverage
 __covbin = $(firstword $(filter $(__covimpl),$(WITH) $(notdir $(shell which $(__covimpl)))))
 
@@ -9,5 +20,6 @@ else ifeq (jscoverage,$(__covbin))
 include $(__JAREDHANSON_MAKE_NODE_DIR)/lib/cov/jscoverage.mk
 endif
 
+# Create directory where coverage reports are saved.
 $(checkstatedir)/cov:
 	mkdir -p $@
